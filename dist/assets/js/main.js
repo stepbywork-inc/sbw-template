ced5b77172ac5943f6daf2d8faa0a42e7dac76dd
uesugi <h.uesugi@step-by.work>
2024-11-03 17:46:16 +0900

    no message

---
+++ dist/assets/js/main.js
@@ -0,0 +1,34 @@
+//ua判定
+document.addEventListener('DOMContentLoaded', function () {
+  AOS.init({
+    //offset: 300,
+    // delay: 100,
+    duration: 1000,
+    once: true,
+  });
+})
+window.addEventListener('load', function () {
+  AOS.refresh();
+})
+window.addEventListener('scroll', function () {
+});
+function pagetop() {
+  $('.js-pagetop').hide();
+  $(window).on('scroll', function () {
+    if ($(this).scrollTop() > 100) {
+      $('.js-pagetop').fadeIn(300);
+    } else {
+      $('.js-pagetop').fadeOut(300);
+    }
+
+    // 指定位置で止めたい場合
+    // var scrollPosition = $(window).height() + $(window).scrollTop(); //現在地
+    // var stopPosition = $('#footer').offset().top; //止めたい位置
+
+    // if (scrollPosition >= stopPosition) {
+    //   $('.js-pagetop').addClass('is-absolute');
+    // } else {
+    //   $('.js-pagetop').removeClass('is-absolute');
+    // }
+  });
+}
---
+++ gulpfile.js
@@ -0,0 +1,103 @@
+//読み込むパスと出力するパスを指定
+const srcRootPath = "dist/lp/20241121/";
+const srcPath = {
+  images: {
+    src: srcRootPath + "assets/_images/",
+    dist: srcRootPath + "assets/images/",
+  },
+};
+//ローカルのパス
+const setting = require("./setting.json");
+const rootPath = setting.rootPath;
+
+const gulp = require("gulp");
+const gulpif = require("gulp-if");
+const browserSync = require("browser-sync");
+const plumber = require("gulp-plumber");
+const newer = require("gulp-newer");
+const mozjpeg = require("imagemin-mozjpeg");
+const imagemin = require("gulp-imagemin");
+const pngquant = require("imagemin-pngquant");
+const del = require("del");
+
+
+const img = () => {
+  return gulp
+    .src(srcPath.images.src + "**/*.{png,apng,jpg,gif,svg,ico}")
+    .pipe(
+      plumber({
+        errorHandler: (err) => {
+          console.log(err);
+          this.emit("end");
+        },
+      })
+    )
+    .pipe(newer(srcPath.images.dist))
+    .pipe(
+      imagemin([
+        pngquant({
+          quality: [0.6, 0.7],
+          speed: 1,
+          floyd: 0,
+        }),
+        mozjpeg({
+          quality: 75,
+          progressive: true,
+        }),
+        imagemin.svgo({
+          removeXMLNS: true,
+          removeDimensions: true,
+          plugins: [
+            {
+              removeAttrs: {
+                attrs: "data.*",
+              },
+            },
+            {
+              removeUnknownsAndDefaults: false,
+            },
+            {
+              removeViewBox: false,
+            },
+          ],
+        }),
+        imagemin.gifsicle({
+          optimizationLevel: 3,
+        }),
+      ])
+    )
+    .pipe(gulp.dest(srcPath.images.dist));
+};
+
+const cleanImg = () =>
+  del(srcPath.images.dist + "**/*.{png,apng,jpg,gif,svg,webp,ico}");
+
+const browserSyncFunc = () => {
+  browserSync({
+    //browser: 'google chrome',
+    proxy: rootPath,
+    notify: false,
+    open: "local",
+    ghostMode: {
+      clicks: false,
+      forms: false,
+      scroll: false,
+    },
+  });
+};
+
+const sync = () => {
+  return gulp
+    .src([
+      srcRootPath + "**/*",
+    ])
+    .pipe(gulpif(browserSync.active === true, browserSync.stream()));
+};
+
+const watch = () => {
+  gulp.watch(srcPath.images.src + "**/*", img);
+  gulp.watch(srcRootPath + "**/*", sync);
+};
+
+exports.default = gulp.parallel(watch, browserSyncFunc);
+exports.build = gulp.series(cleanImg, img);
